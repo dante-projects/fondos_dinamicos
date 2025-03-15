6dbf1daefef1c8024dde87254948319ce429aa5c
dante-projects <ajsantos.082@gmail.com>
2025-03-16 00:55:35 +0100

    feat (componente selectorDesplegable): creacion dinamica de opciones segun fuentes disponibles

--- componentes/selectorDesplegable.js
+++ componentes/selectorDesplegable.js
@@ -10,8 +10,8 @@ class selectorDesplegable extends HTMLElement {
 
         this.shadowRoot.innerHTML += `
             <div class="contenedor borderRadiusGrey">
-                <div class="titulo">Seleccionar fuente
-                    <span id="icono" class="icono material-symbols-outlined centrado">arrow_drop_down</span>
+                <div id="titulo" class="titulo">Seleccionar fuente
+                    <span class="icono material-symbols-outlined centrado">arrow_drop_down</span>
                 </div>
                 <form id="listaFuentes" class="listaFuentes"></form>
             </div>
@@ -47,6 +47,12 @@ class selectorDesplegable extends HTMLElement {
                     height: 32px;
                     color: grey;
                     text-indent: 10px;
+                    cursor: pointer;
+                    transition: .5s .5s;
+
+                    &:hover .icono {
+                        color: red;
+                    }
 
                     .icono {
                         position: absolute;
@@ -55,11 +61,19 @@ class selectorDesplegable extends HTMLElement {
                         aspect-ratio: 1/1;
                         font-size: 30px;
 
-                        &:hover {
-                            color: red;
-                            cursor: pointer;
-                        }
                     }
+                }
+
+                .abierto {
+                    transition: .2s;
+
+                    & .icono {
+                        color: red;
+                    }
+
+                    & + .listaFuentes {
+                        height: 200px;
+                    } 
                 }
 
                 .listaFuentes {
@@ -77,7 +91,13 @@ class selectorDesplegable extends HTMLElement {
                         text-indent: 10px;
 
                         &:has(input:checked) {
-                            background-color: grey;
+                            background-color: rgb(51, 99, 95);
+                            color: white;
+                            transition: .5s;
+                        }
+
+                        &:has(input:not(:checked):hover) {
+                            background-color: rgb(122, 158, 155);
                             color: white;
                         }
 
@@ -97,32 +117,49 @@ class selectorDesplegable extends HTMLElement {
     }
 
     connectedCallback() {
-        const icono = this.shadowRoot.querySelector("#icono")
+        const titulo = this.shadowRoot.querySelector("#titulo")
         const contenedorFuentes = this.shadowRoot.querySelector(".listaFuentes")
 
         function abrirCerrar() {
-            let estado = 0
-            icono.classList.toggle("abierto")
-            if (icono.classList.contains("abierto")) {
-                estado = 1
-                contenedorFuentes.style.height = "200px"
-            } else {
-                estado = 0
-                contenedorFuentes.style.height = "0"
-            }
+            titulo.classList.toggle("abierto")
+            const estado = titulo.classList.contains("abierto") ? 1 : 0
             return estado
         }
 
-        icono.addEventListener("click", () => {
+        titulo.addEventListener("click", () => {
             abrirCerrar()
         })
 
-        // eventos personalizado despues de la carga completa
+        function obtenerFuentes(item) {
+            const fuentesJson = JSON.parse(localStorage.getItem(item))
+            const fuentes = fuentesJson.fuentes
+            return fuentes
+        }
 
+        function crearOpcion(array) {
+            array.forEach((item, num) => {
+                const nuevaOpcion = document.createElement("span")
+                nuevaOpcion.innerText = item.split("=")[1].replace("+", " ")
+                nuevaOpcion.classList.add("fuente")
+    
+                const input = document.createElement("input")
+                input.setAttribute("type", "radio")
+                input.setAttribute("name", "fuente")
+                input.checked = num === 0 ? true : null
+
+                nuevaOpcion.appendChild(input)
+                contenedorFuentes.appendChild(nuevaOpcion)
+            })
+        }
+
+        // eventos personalizado despues de la carga completa
         window.addEventListener("DOMContentLoaded", () => {
+
             window.dispatchEvent(new CustomEvent("selectorFuentes"))
+
             window.addEventListener("fuentesGoogle", () => {
-                console.log("COPMONENTE: respuesta recibida")
+                console.log("COMPONENTE: respuesta recibida")
+                crearOpcion(obtenerFuentes("fuentesGoogle"))            
             })
         })
 
